6adc4bbd591d6d3331ec2847f4b76b21f35a597d
jkutner <jkutner@auric.local>
2009-08-29 14:00:10 -0500

    fixed up several rules and created a Discount helper class

--- app/rules/store_rulebook.rb
+++ app/rules/store_rulebook.rb
@@ -5,20 +5,21 @@ class StoreRulebook < Ruleby::Rulebook
   def customer_discounts    
     # New customers get 10% discount     
     rule :newCustomerDiscount, [User, :u, m.order_count(&c{|c| c==0 or c==nil})], [Product, :p] do |v|
-      if v[:p].discount
-        v[:p].discount += 10
-      else
-        v[:p].discount = 10
-      end
+      assert Discount.new(10)
     end
     
     # Customers who have purchased more than 5 times get a 5% discount 
     rule :loyalCustomerDiscount, [User, :u, m.order_count >= 5], [Product, :p] do |v|
-      if v[:p].discount
-        v[:p].discount += 5
+      assert Discount.new(5)
+    end
+    
+    rule :applyDiscounts, [Product, :p], [Discount, :d] do |v|
+      if v[:p].discount and v[:d].is_cummulative
+        v[:p].discount += v[:d].amount
       else
-        v[:p].discount = 5
+        v[:p].discount = v[:d].amount
       end
+      retract v[:d]
     end
   end
   
@@ -27,12 +28,18 @@ class StoreRulebook < Ruleby::Rulebook
     discounted_products.each do |product_name|
       rule_name = "fifteenPercentOffSale_#{product_name}"
       rule rule_name.to_sym, [Product, :p, m.name == product_name] do |v|
-        if v[:p].discount
-          v[:p].discount += 15
-        else
-          v[:p].discount = 15
-        end
+        assert Discount.new(15)
       end      
     end    
   end  
+end
+
+class Discount
+  attr :amount
+  attr :is_cummulative
+  
+  def initialize(amount, is_cummulative=true)
+    @amount = amount
+    @is_cummulative = is_cummulative
+  end
 end
